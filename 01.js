647530e190592382c1f47db26e11b8b0e041fa4b
Ahmad Hassan <ahmadmian803@gmail.com>
2024-01-11 17:26:59 +0500

    leet-code

---
+++ 01.js
@@ -0,0 +1,15 @@
+let nums = [4,9,8,6,7,3,4,5,2,9];
+
+function checknum(nums){
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1 ; j<nums.length ; j++){
+        if(nums[i] === nums[j]){
+            return true;
+        }
+    }
+}
+    return false
+}
+
+let result = checknum(nums);
+console.log(result);
