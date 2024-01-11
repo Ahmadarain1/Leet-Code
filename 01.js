f46f0fe6c435eb26ce31c2dd3cb7802da4a39836
Ahmad Hassan <ahmadmian803@gmail.com>
2024-01-11 20:07:14 +0500

    PR Feedback

--- 01.js
+++ 01.js
@@ -1,7 +1,7 @@
 let nums = [4,9,8,6,7,3,4,5,2,9];
 
 function checknum(nums){
-    for(let i=0;i<nums.length;i++){
+    for(let i=0;i<nums.length-1;i++){
         for(let j=i+1 ; j<nums.length ; j++){
         if(nums[i] === nums[j]){
             return true;
